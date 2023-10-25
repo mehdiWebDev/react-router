7f99cdd286ad7d4b19d412d423a860c4b1aa1b53
mahdi <mahdi@nixa.ca>
2023-10-25 11:42:28 -0400

    add news letter sign up and useFetcher to load data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,70 @@
-// Challenge / Exercise
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventsDetails';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
+import { Action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/Root";
-import Home from "./pages/Home";
-import Events,{loader as eventsLoader} from "./pages/Events";
-import EventsDetails,{loader as eventDetailLoader,action as deleteEventAction } from "./pages/EventsDetails";
-import NewEvent from "./pages/NewEvent";
-import EditEvent from "./pages/EditEvent";
-import Error from "./pages/Error";
-import EventRoot from './pages/EventsRoot';
-
-import {Action as EventAction} from './components/EventForm';
-
-
-
-const route = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />,
-    errorElement : <Error/>,
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Home />},
-      { path: 'events',element: <EventRoot/>, children: [
-
-        { path: '', element: <Events />,loader: eventsLoader },
-
-        {
-          path:':eventid',
-          id : 'event-detail',
-          loader: eventDetailLoader,
-          action: deleteEventAction,
-          children: [
-              { path: '', element: <EventsDetails/> },
-              { path: 'edit',element:<EditEvent/>, action:EventAction  } 
-            
-          ]
-        },
-
-        { path: 'new',element:<NewEvent/>,action:EventAction },
-       
-
-      ] },
-
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventid',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
     ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={route} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/pages/EventsDetails.jsx
+++ frontend/src/pages/EventsDetails.jsx
@@ -1,48 +1,79 @@
-
-import EventItem from '../components/EventItem';
-import { redirect, useRouteLoaderData } from 'react-router-dom';
+import EventItem from "../components/EventItem";
+import { Await, defer, redirect, useRouteLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventsDetails() {
+  const { event, events } = useRouteLoaderData("event-detail");
 
-  const data = useRouteLoaderData('event-detail');
-    
   return (
     <>
-    <EventItem event={data.event} />
+    <Suspense fallback="loading...">
+      <Await resolve={event}>
+          {loadEvent => <EventItem event={loadEvent} />}
+        </Await>
+    </Suspense>
+
+    <Suspense fallback="loading...">
+      <Await resolve={events}>
+          {loadEvents =>   <EventsList events={loadEvents}/> }
+      </Await>
+
+    </Suspense>
+
     </>
-
-  )
+  );
 }
 
-export async function loader({request,params}){
-
-  const id = params.eventid
-
-  const response =  await fetch('http://localhost:8080/events/'+ id);
-  if(!response.ok){
-    throw new Response(JSON.stringify({message:'could not fetch event'}),{status:500})
-  }else {
-    return response;
+async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "could not fetch event" }), {
+      status: 500,
+    });
+  } else {
+    const resData = await response.json();
+    return resData.event;
   }
-
 }
 
-export async function action ({params,request}){
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    // return {isError:true,message:"Could not fetch events"}
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 500,
+    });
+  } else {
+    const resData = await response.json();
+    console.log(resData.events);
+    return resData.events;
+  }
+}
 
+export async function loader({ request, params }) {
+  console.log(params.eventid);
+  return defer({
+    event: await loadEvent(params.eventid),
+    events: loadEvents(),
+  });
+}
+
+export async function action({ params, request }) {
   console.log(params);
 
   const eventId = params.eventid;
-console.log(eventId);
+  console.log(eventId);
 
- const response = await fetch('http://localhost:8080/events/'+eventId, {
-  method:request.method,
-  
- })
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
 
- if(!response.ok){
-  throw new Response(JSON.stringify({message:'could not delete event'}),{status:500})
-}else {
-  return redirect('/events');
-}
-
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "could not delete event" }), {
+      status: 500,
+    });
+  } else {
+    return redirect("/events");
+  }
 }
